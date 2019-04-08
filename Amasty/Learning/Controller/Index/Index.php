ba2fd14e7d76d79061fc0a94be26a96110759231
Vlad <baraulya16@gmail.com>
2019-04-08 13:41:00 +0300

    first part of homework

---
+++ Amasty/Learning/Controller/Index/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Amasty\Learning\Controller\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
---
+++ Amasty/Learning/registration.php
@@ -0,0 +1,4 @@
+<?php \Magento\Framework\Component\ComponentRegistrar::register(
+\Magento\Framework\Component\ComponentRegistrar::MODULE, 'Amasty_Learning',
+__DIR__
+);
