d52372a0e14f0039e5f98d8c0a8a7c3ceb71ba72
Барауля Влад <49244760+VladBaraulya@users.noreply.github.com>
2019-04-25 19:19:58 +0300

    second commit

--- Amasty/Learning/Controller/Index/Index.php
+++ Amasty/Learning/Controller/Index/Index.php
@@ -9,7 +9,12 @@ class Index extends \Magento\Framework\App\Action\Action
 
     public function execute()
     {
+
+
         $this->_view->loadLayout();
+
         $this->_view->renderLayout();
     }
+
+
 }
